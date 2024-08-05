601758a074bfdd409d41c289278bff77fdc58020
Rowan d'Auria <56480877+escucheon1@users.noreply.github.com>
2024-08-05 12:57:37 +0100

    Refactor controller to create service dependency

--- src/main/java/com/rowan.weather/weather_app/ForecastController.java
+++ src/main/java/com/rowan.weather/weather_app/ForecastController.java
@@ -1,15 +1,23 @@
 package com.rowan.weather.weather_app;
 
+import com.rowan.weather.weather_app.service.ForecastService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
-public class HomeController {
+public class ForecastController {
+
+    private final ForecastService service;
+
+    public ForecastController(ForecastService service) {
+        this.service = service;
+    }
 
     @RequestMapping("/")
     public @ResponseBody String greeting() {
-        return "Hello, World";
+        return service.greet();
     }
 
 }
---
+++ src/main/java/com/rowan/weather/weather_app/service/ForecastService.java
@@ -0,0 +1,12 @@
+package com.rowan.weather.weather_app.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ForecastService {
+
+    public String greet() {
+        return "Hello World";
+    }
+
+}
