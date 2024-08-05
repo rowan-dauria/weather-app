8fbecf57c340fcdb47549afc0a41ab4e43b0f7fc
Rowan d'Auria <56480877+escucheon1@users.noreply.github.com>
2024-08-05 10:42:40 +0100

    adding Spring Boot server boilerplate

---
+++ src/main/java/com/rowan_d_weather_app/weather_app/WeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.rowan_d_weather_app.weather_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rowan_d_weather_app/weather_app/WeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rowan_d_weather_app.weather_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
