b4297b8e6264f77e765ee6140cc33ca9b7341eb7
Rowan d'Auria <56480877+escucheon1@users.noreply.github.com>
2024-10-09 21:50:33 +0100

    Remove changes to tests to more appropriately read JSON data from file in future commits

--- src/test/java/com/rowan/weather/weather_app/MetOfficeClientTests.java
+++ src/test/java/com/rowan/weather/weather_app/MetOfficeClientTests.java
@@ -7,16 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS) // Needed to make @beforeall not static
 @RestClientTest(MetOfficeClient.class)
@@ -30,20 +25,18 @@ public class MetOfficeClientTests {
     @BeforeAll
     void setup() throws IOException {
 
-        byte[] bytes = Files.readAllBytes(Paths.get("C:\\Users\\Rowan d'Auria\\Documents\\GitHub\\weather-app\\src\\test\\java\\com\\rowan\\weather\\weather_app\\dummy_data\\hourly-sample.json"));
-        mockServer.expect(requestTo("sitespecific/v0/point/hourly?latitude=51.507351&longitude=-0.127758"))
-                .andRespond(withSuccess(bytes, MediaType.APPLICATION_JSON));
     }
-
+    // TODO Make the service implement the request to the metoffice API so that the metoffice server can be mocked
+    // TODO Make the mock server read the data from a dummy JSON file taken from metoffice website. This will enable me to make an appropriate POJO
     @Test
     void shouldHaveMethodsByPeriod() {
         ForecastDataPOJO dailyForecast = this.client.getSiteSpecificDailyForecast(51.507351, -0.127758);
         assertThat(dailyForecast.getData()).isEqualTo(5);
 
         ForecastDataPOJO threeHourlyForecast = this.client.getSiteSpecificThreeHourlyForecast(51.507351, -0.127758);
-        assertThat(dailyForecast.getData()).isEqualTo(5);
+        assertThat(threeHourlyForecast.getData()).isEqualTo(5);
 
         ForecastDataPOJO hourlyForecast = this.client.getSiteSpecificHourlyForecast(51.507351, -0.127758);
-        assertThat(dailyForecast.getData()).isEqualTo(5);
+        assertThat(hourlyForecast.getData()).isEqualTo(5);
     }
 }
