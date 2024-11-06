c1ba3a1ed4e2ef06401a5a57553b656d74e69557
Rowan d'Auria <56480877+escucheon1@users.noreply.github.com>
2024-11-06 18:28:57 +0000

    Made functional met office client and updated tests. Reformatted dependency injections.

--- src/main/java/com/rowan.weather/weather_app/service/MetOfficeClient.java
+++ src/main/java/com/rowan.weather/weather_app/service/MetOfficeClient.java
@@ -1,34 +1,27 @@
 package com.rowan.weather.weather_app.service;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
-
-import jakarta.annotation.PostConstruct;
 
 @Service
 public class MetOfficeClient {
 
     // https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/hourly?latitude=51.507351&longitude=-0.127758
 
-    private RestClient restClient;
+    private final RestClient restClient;
 
-    @Autowired
     private final MetClientConfig metClientConfig;
 
-    public MetOfficeClient(MetClientConfig metClientConfig) {
-        this.metClientConfig = metClientConfig;
-    }
-
-    @PostConstruct
-    public void init() {
-        restClient = RestClient.builder()
+    // Cannot add the api key as a default header because the config properties have not been set at
+    // the point of client instantiation. Additionally, if the configuration of restClient is put in a
+    // @PostConstruct method, the MockRestServiceServer in the test class cannot be autoconfigured because
+    // Spring Boot expects the RestClient to be injected in the constructor.
+    @Autowired
+    public MetOfficeClient(MetClientConfig config, RestClient.Builder builder) {
+        metClientConfig = config;
+        restClient = builder
                 .baseUrl("https://data.hub.api.metoffice.gov.uk/")
-                .defaultHeader("apikey", metClientConfig.getApiKey())
                 .build();
     }
 
@@ -41,11 +34,12 @@ public class MetOfficeClient {
     }
 
     public String getSiteSpecificHourlyForecast(final double latitude, final double longitude) {
-        String uri = String.format("sitespecific/v0/point/hourly?latitude=%f1&longitude=%f", latitude, longitude);
+        String uri = String.format("sitespecific/v0/point/hourly?latitude=%f&longitude=%f", latitude, longitude);
         try {
             return restClient
                     .get()
                     .uri(uri)
+                    .header("apikey", metClientConfig.getApiKey())
                     .retrieve()
                     .body(String.class);
         } catch (Exception e) {
--- src/test/java/com/rowan/weather/weather_app/MetOfficeClientTests.java
+++ src/test/java/com/rowan/weather/weather_app/MetOfficeClientTests.java
@@ -1,22 +1,31 @@
 package com.rowan.weather.weather_app;
 
 import com.rowan.weather.weather_app.service.ForecastDataPOJO;
+import com.rowan.weather.weather_app.service.MetClientConfig;
 import com.rowan.weather.weather_app.service.MetOfficeClient;
+import jakarta.annotation.PostConstruct;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.context.annotation.*;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.client.ExpectedCount;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.web.client.RestClient;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UncheckedIOException;
+import java.lang.reflect.Method;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -24,6 +33,7 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS) // Needed to make @beforeall not static
+@ContextConfiguration(classes = {MetOfficeClient.class, MetClientConfig.class})
 @RestClientTest(MetOfficeClient.class)
 public class MetOfficeClientTests {
     @Autowired
@@ -49,21 +59,60 @@ public class MetOfficeClientTests {
 
     @BeforeAll
     void setup() {
-        String requestURI = String.format("sitespecific/v0/point/hourly?latitude=%f&longitude=%f", coords[0], coords[1]);
-        mockServer.expect(requestTo(requestURI))
+        String requestURI = String.format(
+                "https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/hourly?latitude=%f&longitude=%f",
+                coords[0],
+                coords[1]
+        );
+        // todo move this mockserver expect so that it is test dependent
+        mockServer.expect(ExpectedCount.times(1), requestTo(requestURI))
                 .andRespond(withSuccess(asString(hourlyResource), MediaType.APPLICATION_JSON));
+    }
+    @Test
+    void checkMethodsExist() {
+        Method dailyMethod = null;
+        Method threeHourlyMethod = null;
+        Method hourlyMethod = null;
+        try {
+            dailyMethod = client.getClass().getMethod("getSiteSpecificDailyForecast", double.class, double.class);
+        } catch (NoSuchMethodException e) {
+            System.err.println(e.getMessage());
+        }
+        try {
+            threeHourlyMethod = client.getClass().getMethod("getSiteSpecificDailyForecast", double.class, double.class);
+        } catch (NoSuchMethodException e) {
+            System.err.println(e.getMessage());
+        }
+        try {
+            hourlyMethod = client.getClass().getMethod("getSiteSpecificDailyForecast", double.class, double.class);
+        } catch (NoSuchMethodException e) {
+            System.err.println(e.getMessage());
+        }
+
+        assertThat(dailyMethod).isNotNull();
+        assertThat(threeHourlyMethod).isNotNull();
+        assertThat(hourlyMethod).isNotNull();
     }
     // TODO Make the service implement the request to the metoffice API so that the metoffice server can be mocked
     // TODO Make the mock server read the data from a dummy JSON file taken from metoffice website. This will enable me to make an appropriate POJO
     @Test
-    void shouldHaveMethodsByPeriod() {
+    void shouldMakeRequestToServer() {
         ForecastDataPOJO dailyForecast = this.client.getSiteSpecificDailyForecast(coords[0], coords[1]);
         assertThat(dailyForecast.getData()).isEqualTo(5);
 
         ForecastDataPOJO threeHourlyForecast = this.client.getSiteSpecificThreeHourlyForecast(51.507351, -0.127758);
         assertThat(threeHourlyForecast.getData()).isEqualTo(5);
 
-        ForecastDataPOJO hourlyForecast = this.client.getSiteSpecificHourlyForecast(51.507351, -0.127758);
-        assertThat(hourlyForecast.getData()).isEqualTo(5);
+        String hourlyForecast = this.client.getSiteSpecificHourlyForecast(coords[0], coords[1]);
+        try {
+            final JSONObject foreCastObj = new JSONObject(hourlyForecast);
+            final String type = foreCastObj.getString("type");
+            assertThat(type).isEqualTo("FeatureCollection");
+        } catch (JSONException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException("There was an error reading the hourly forecast JSON");
+        } catch (NullPointerException e) {
+            throw new RuntimeException("Could not find property 'type' in JSON object");
+        }
     }
 }
